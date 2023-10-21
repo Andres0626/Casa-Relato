d551153b05934cc0c151150b2d8c3fb4ea039414
Andres Ramirez Ruiz <andres.ramirez-ru@uniminuto.edu.co>
2023-10-21 11:48:19 -0500

    se agrega vista de aliados funcionalidad y conexion con bd

---
+++ casa-relato/Controllers/AliadosController.cs
@@ -0,0 +1,73 @@
+﻿using casa_relato.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace casa_relato.Controllers
+{
+    public class AliadosController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AliadosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public int IdAliado { get; private set; }
+        public string NombreAliado { get; private set; }
+        public string DescripcionAliado { get; private set; }
+        public string ImagenAliado { get; private set; }
+
+        public ActionResult Index()
+        {
+            try
+            {
+
+
+
+                string Connection = _configuration.GetConnectionString("ConnectionStrings");
+                var Aliados = new List<AliadosModel>();
+
+                using (SqlConnection connections = new SqlConnection(Connection))
+                {
+                    string command = $@"Select * from Aliados";
+
+                    using (SqlCommand cmd = new SqlCommand(command, connections))
+                    {
+                        connections.Open();
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+
+                                var aliados = new AliadosModel
+                                {
+                                    IdAliado = (int)reader["IdAliado"],
+                                    NombreAliado = (string)reader["NombreAliado"],
+                                    DescripcionAliado = (string)reader["DescripcionAliado"],
+                                    ImagenAliado = (string)reader["ImagenAliado"]
+
+                                };
+
+                                Aliados.Add(aliados);
+
+                            }
+
+                        }
+                    }
+
+                }
+
+
+                return View(Aliados);
+            }
+
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+    }
+}
