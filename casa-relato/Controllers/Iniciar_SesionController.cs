5ca0a0c7b088ef41fc92f5b17419cd5a11ee92c9
Andres Ramirez Ruiz <andres.ramirez-ru@uniminuto.edu.co>
2023-10-26 20:13:09 -0500

    Se configura inicio sesion

--- casa-relato/Controllers/Iniciar_SesionController.cs
+++ casa-relato/Controllers/Iniciar_SesionController.cs
@@ -1,13 +1,60 @@
 ﻿
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
 
 namespace casa_relato.Controllers
 {
     public class Iniciar_SesionController : Controller
     {
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        private readonly IConfiguration _configuration;
+
+        public Iniciar_SesionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult OnPost(string username, string password)
+        {
+            try
+            {
+                string Connection = _configuration.GetConnectionString("ConnectionStrings");
+                using (var connection = new SqlConnection(Connection))
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password";
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.Add(new SqlParameter("@Username", username));
+                        command.Parameters.Add(new SqlParameter("@Password", password));
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                return View();
+                            }
+                            else
+                            {
+                                return View(reader);
+                            }
+                        }
+                    }
+                }
+
+                }catch (Exception ex) 
+            
+            {
+                
+
+            }
             return View();
         }
     }
